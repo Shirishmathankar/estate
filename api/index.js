89e0028deb975863edd45319fd67d5cbe4ec4b33
shirish mathankar <shirishmathankar38@gmail.com>
2024-10-30 03:02:29 -0700

    connect to data base

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from "express"
-
+import mongoose from "mongoose"
+import dotenv  from "dotenv";
+dotenv.config();
 const app=express();
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+  console.log("connect to mongodb")
+})
+.catch((err)=>{
+  console.log(err)
+})
 
 app.listen(3000,()=>{
-  console.log(`app is listen on port${3000}`)
+   console.log(`app is listen on port${3000}`)
 }
 )
