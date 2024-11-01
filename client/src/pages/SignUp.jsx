fee06fa9aae9c3c2c7955dc6f5790f1cc94cc387
shirish mathankar <shirishmathankar38@gmail.com>
2024-11-01 10:24:40 -0700

    create sign in functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Header from '../components/Header'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      <Header/>
+    <>
+    <Header/>
+    <div className='p-3 max-w-lg mx-auto'>
+      
+      <h1 className='text-3xl font-semibold capitalize text-center my-7'>sign up</h1>
+      <form className='flex flex-col gap-3 '>
+         <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
+         <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/>
+         <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
+         <button className='my-3 border p-3 rounded-lg bg-slate-600 hover:opacity-95 disabled:opacity-80 uppercase text-white' >sign in</button>
+      </form>
+      <div className='capitalize flex gap-2 mt-2'>
+        <p>already have an account?</p>
+       <Link to='/sign-in'><span className='text-blue-500 hover:underline '>sign in</span></Link>
+      </div>
     </div>
+    </>
   )
 }
 
