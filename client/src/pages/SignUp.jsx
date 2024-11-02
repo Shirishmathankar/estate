b0cbbed6f0932d2cb8535e5501a880f2b712db23
shirish mathankar <shirishmathankar38@gmail.com>
2024-11-02 04:20:22 -0700

    create signup fuctionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData,setformData]=useState({});
+  const [Error,setError]=useState(null);
+  const [Loading,setLoading]=useState(false);
+  const nevigate=useNavigate();
+  
+  const handleInput=(e)=>{
+    
+    setformData({
+       ...formData,
+       [e.target.id]:e.target.value,
+    });
+  }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+    
+      if(!formData.username||!formData.email||!formData.password){
+        setError("all feilds are mandetory")
+        setLoading(false)
+        return
+      }
+      try {
+        setLoading(true);
+        setError(null); // Reset error before submission
+    
+        const res = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+    
+        const data=await res.json();
+        
+        if (data.success === false) {
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
+    
+        setLoading(false);
+        setError(null);
+        nevigate('/sign-in');
+    
+      } catch (error) {
+        setError(error.message || "An error occurred. Please try again.");
+        setLoading(false);
+      }
+    };
+    
   return (
     <>
     <Header/>
     <div className='p-3 max-w-lg mx-auto'>
       
       <h1 className='text-3xl font-semibold capitalize text-center my-7'>sign up</h1>
-      <form className='flex flex-col gap-3 '>
-         <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
-         <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/>
-         <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
-         <button className='my-3 border p-3 rounded-lg bg-slate-600 hover:opacity-95 disabled:opacity-80 uppercase text-white' >sign in</button>
+      <form className='flex flex-col gap-3 ' onSubmit={handleSubmit}>
+         <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg' onChange={handleInput}/>
+         <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg' onChange={handleInput}/>
+         <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg' onChange={handleInput}/>
+         <button disabled={Loading} className='my-3 border p-3 rounded-lg bg-slate-600 hover:opacity-95 disabled:opacity-80 uppercase text-white' >{Loading?"Loading...":"sign up"}</button>
       </form>
       <div className='capitalize flex gap-2 mt-2'>
         <p>already have an account?</p>
        <Link to='/sign-in'><span className='text-blue-500 hover:underline '>sign in</span></Link>
-      </div>
+        
+      </div> 
+      {Error&&<p className='text-red-600'>{Error}</p>}
     </div>
     </>
   )
