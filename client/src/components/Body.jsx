61f1423e3fbb3d77203cdc6c846bc4fb6632cb1e
shirish mathankar <shirishmathankar38@gmail.com>
2024-11-10 03:09:38 -0800

    file mistake

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -5,7 +5,7 @@ import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import About from '../pages/About'
 import Profile from '../pages/Profile'
-import Private from './private'
+import Private from './Private'
 
 
 const Body = () => {
